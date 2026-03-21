1c9a2dec2f36e4c2ffbeb73e1ef786739989222c
krishguptaofficial <krishguptaofficial1@gmail.com>
2026-03-21 15:42:46 +0530

    feat: add phone normalization to avoid messy data'

--- app/core/hashing.py
+++ app/core/hashing.py
@@ -4,11 +4,27 @@ from app.config import settings
 import secrets
 
 
+def normalize_phone(phone:str):
+    phone = phone.strip().replace(" ","").replace("-","")
+
+    if phone.startswith("+91"):
+        phone = phone[3:]
+
+    elif phone.startswith("91") and len(phone)==12:
+        phone = phone[2:]
+
+    if len(phone)!=10 or not phone.isdigit():
+        raise ValueError("Invalid Phone Number")
+    return phone
+    
+
+
 def generate_network_token(phone: str, member_id: int) -> str:
     """
     Identifies a specific family member across the entire Tap2Med network.
     Input: '9810012345' + 1 (Father) vs '9810012345' + 2 (Mother).
     """
+    phone = normalize_phone(phone)
    
     combined_input = f"{phone}:{member_id}"
     
@@ -24,6 +40,7 @@ def generate_local_token(phone: str, member_id: int, clinic_salt: str) -> str:
     Identifies a specific family member ONLY within one specific clinic.
     The clinic_salt ensures this hash is useless to any other clinic.
     """
+    phone = normalize_phone(phone)
    
     combined_key = f"{settings.SECRET_KEY}:{clinic_salt}"
    
@@ -41,6 +58,7 @@ def generate_identity_tokens(phone: str, member_id: int, clinic_salt: str) -> di
     Convenience wrapper for the API. 
     Returns the 'Ghosts' that will be stored in PostgreSQL.
     """
+    phone = normalize_phone(phone)
     return {
         "network_token": generate_network_token(phone, member_id), 
         "local_token": generate_local_token(phone, member_id, clinic_salt) 
