3989596b5726a78eba0e8cdbba4a1e083aebddfc
krishguptaofficial <krishguptaofficial1@gmail.com>
2026-04-17 14:53:22 +0530

    alembic fixes

--- alembic/env.py
+++ alembic/env.py
@@ -1,31 +1,47 @@
-import sys 
+import sys
 import os
-from alembic import context      #type: ignore
-from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 
-sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
+from sqlalchemy import engine_from_config, pool
+from alembic import context #type:ignore
 
-from app.db.database import Base
-import app.db.models
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, BASE_DIR)
+
+
 from dotenv import load_dotenv
 
-load_dotenv()
+ENV_PATH = os.path.join(BASE_DIR, ".env")
+load_dotenv(ENV_PATH)
+
+
+from app.db.database import Base
+import app.db.models  
+
 
 config = context.config
+
+database_url = os.getenv("DATABASE_URL")
+
+if not database_url:
+    raise RuntimeError("DATABASE_URL not found in .env")
+
+config.set_main_option("sqlalchemy.url", database_url)
+
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+
 target_metadata = Base.metadata
 
-database_url = os.getenv("DATABASE_URL")
-if database_url:
-    config.set_main_option("sqlalchemy.url", database_url)
+
 
 
 def run_migrations_offline() -> None:
-    
     url = config.get_main_option("sqlalchemy.url")
+
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -37,8 +53,8 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
+
 def run_migrations_online() -> None:
-    
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
@@ -47,8 +63,15 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata,
         )
 
         with context.begin_transaction():
             context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
