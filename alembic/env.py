2cafed381f1a87bf11d52fee9c319765f3f44bb6
krishguptaofficial <krishguptaofficial1@gmail.com>
2026-04-17 12:35:28 +0530

    alembic setup

--- alembic/env.py
+++ alembic/env.py
@@ -4,10 +4,13 @@ from alembic import context      #type: ignore
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 
+sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app.db.database import Base
 import app.db.models
+from dotenv import load_dotenv
 
-sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
+load_dotenv()
 
 config = context.config
 if config.config_file_name is not None:
@@ -15,6 +18,37 @@ if config.config_file_name is not None:
 
 target_metadata = Base.metadata
 
+database_url = os.getenv("DATABASE_URL")
+if database_url:
+    config.set_main_option("sqlalchemy.url", database_url)
 
 
+def run_migrations_offline() -> None:
+    
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
 
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
