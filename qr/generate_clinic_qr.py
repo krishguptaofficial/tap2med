2f469112486d9a3abce5f16ea4bab744466eb7ab
krishguptaofficial <krishguptaofficial1@gmail.com>
2026-04-19 21:57:06 +0530

    qr genration logic setup

---
+++ qr/generate_clinic_qr.py
@@ -0,0 +1,29 @@
+import qrcode
+import os
+
+def generate_clinic_qr(clinic_id: str, base_url: str = "https://tap2med.com/scan?clinic="):
+   
+    data = f"{base_url}{clinic_id}"
+    
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    
+    qr.add_data(data)
+    qr.make(fit=True)
+    
+    img = qr.make_image(fill_color="black", back_color="white")
+    
+    
+    folder = "clinic_qrs"
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+        
+    file_path = os.path.join(folder, f"clinic_{clinic_id}.png")
+    img.save(file_path)
+    
+    print(f"Generated QR for Clinic ID {clinic_id} at {file_path}")
+    return file_path
