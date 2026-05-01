0143620f1e413dfd3378396db3766307cb5d938a
krishguptaofficial <krishguptaofficial1@gmail.com>
2026-05-01 16:06:03 +0530

    fix: alembic fixes

--- alembic/env.py
+++ alembic/env.py
@@ -1,53 +1,34 @@
-import sys
 import os
-from os import path
+import sys
+from logging.config import fileConfig
+from sqlalchemy import engine_from_config, pool
+from alembic import context #type:ignore
 from dotenv import load_dotenv
-from app.db.database import Base
 
 
-sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+sys.path.append(os.getcwd())
 
 load_dotenv()
 
-target_metadata = Base.metadata
+from app.db.database import Base 
+from app.db.models import Clinic, Event, Prescription           
 
+_ = [Clinic, Event, Prescription]
 
-from logging.config import fileConfig
-
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
-from alembic import context #type:ignore
-
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
+target_metadata = Base.metadata
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode."""
     url = os.getenv("DATABASE_URL")
-    
-    # 2. Configure the context
     context.configure(
         url=url,
-        target_metadata=Base.metadata,
+        target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
     )
@@ -55,20 +36,16 @@ def run_migrations_offline() -> None:
     with context.begin_transaction():
         context.run_migrations()
 
-
 def run_migrations_online() -> None:
-   
+    
     db_url = os.getenv("DATABASE_URL")
     
-    # If the URL isn't found, the migration should fail early
-    if not db_url:
-        raise ValueError("DATABASE_URL not found in .env file")
-
+   
     configuration = config.get_section(config.config_ini_section, {})
     
     connectable = engine_from_config(
         configuration,
-        url=db_url, 
+        url=db_url,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
@@ -81,7 +58,6 @@ def run_migrations_online() -> None:
 
         with context.begin_transaction():
             context.run_migrations()
-
 
 if context.is_offline_mode():
     run_migrations_offline()
