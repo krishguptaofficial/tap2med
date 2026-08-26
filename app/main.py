3baa9226f3ccceb904f88d4e6e04aac2b72c51d1
krishguptaofficial <krishguptaofficial1@gmail.com>
2026-08-26 18:37:57 +0530

    added staff dashboard

--- app/main.py
+++ app/main.py
@@ -74,6 +74,11 @@ def serve_staff_login():
     logger.info("Staff Login Opened")
     return FileResponse("frontend/clinic/staff_login.html")
 
+@app.get("/staff-dashboard")
+def serve_staff_dashboard():
+    logger.info("Staff Dashboard Opened")
+    return FileResponse("frontend/clinic/staff_dashboard.html")
+
 @app.get("/scan")
 def serve_patient_scan():
     logger.info("Scan Page Opened")
